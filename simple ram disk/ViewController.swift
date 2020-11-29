8a89e740175d74ee77380d59035ed0bf026a33dd
Alexander A. Kropotin <ololx@icloud.com>
2020-11-29 20:42:48 +0500

    feature(simple ram disk) init project

---
+++ simple ram disk/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  simple ram disk
+//
+//  Created by Alexander A. Kropotin on 29.11.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
