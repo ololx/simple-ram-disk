284a8c23c225c607303596aeb3b95c8269a6d048
Alexander A. Kropotin <ololx@icloud.com>
2021-01-14 17:51:04 +0500

    feature(VolumeDetail.swift) init volume detail class

---
+++ simple ram disk/entity/VolumeDetail.swift
@@ -0,0 +1,19 @@
+//
+//  VolumeDetail.swift
+//  simple ram disk
+//
+//  Created by Alexander A. Kropotin on 14.01.2021.
+//
+
+public struct VolumeDetail {
+    
+    //
+    // The volume name for file system
+    //
+    private var name: String!
+    
+    //
+    // The volume size in messure
+    //
+    private var size: Int64!
+}
